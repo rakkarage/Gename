6bc293d7b3309a9060b1776c39c82bf23cabb4eb
Rakka Rage <rakkarage@gmail.com>
2023-06-26 10:12:08 -0400

    x

--- Gename.gd
+++ Gename.gd
@@ -1,20 +1,20 @@
 class_name Gename
 
-var oneWord := true
-var maleChance := 0.5
-var neutralEnable := true
-var neutralChance := 0.1
-var generatedEnable := true
-var generatedChance := 0.5
-var preChance := 0.333
-var vowelChance := 0.8
-var middle0Chance := 0.3
-var middle1Chance := 0.1
-var postChance := 0.22
+var one_word := true
+var male_chance := 0.5
+var neutral_enable := true
+var neutral_chance := 0.1
+var generated_enable := true
+var generated_chance := 0.5
+var pre_chance := 0.333
+var vowel_chance := 0.8
+var middle0_chance := 0.3
+var middle1_chance := 0.1
+var post_chance := 0.22
 const _error := "Error: "
-const _namePath := "res://Gename/Name.txt"
-const _syllablePath := "res://Gename/Syllable.txt"
-const _titlePath := "res://Gename/Title.txt"
+const _name_path := "res://Gename/Name.txt"
+const _syllable_path := "res://Gename/Syllable.txt"
+const _title_path := "res://Gename/Title.txt"
 const _vowel := ['a', 'e', 'i', 'o', 'u', 'y']
 var _data := {
 	"male": { "name": [], "start": [], "middle": [], "finish": [], "pre": [], "post": [] },
@@ -25,9 +25,9 @@ var _rng := RandomNumberGenerator.new()
 
 func _init() -> void:
 	_rng.randomize()
-	var file := FileAccess.open(_namePath, FileAccess.READ)
+	var file := FileAccess.open(_name_path, FileAccess.READ)
 	if !file:
-		print_debug(_error + _namePath)
+		print_debug(_error + _name_path)
 		return
 	while !file.eof_reached():
 		var csv := file.get_csv_line()
@@ -45,9 +45,9 @@ func _init() -> void:
 			if value != "":
 				_data.neutral.name.append(value)
 	file.close()
-	file = FileAccess.open(_syllablePath, FileAccess.READ)
+	file = FileAccess.open(_syllable_path, FileAccess.READ)
 	if !file:
-		print_debug(_error + _syllablePath)
+		print_debug(_error + _syllable_path)
 		return
 	while !file.eof_reached():
 		var csv := file.get_csv_line()
@@ -89,9 +89,9 @@ func _init() -> void:
 			if value != "":
 				_data.neutral.finish.append(value)
 	file.close()
-	file = FileAccess.open(_titlePath, file.READ)
+	file = FileAccess.open(_title_path, file.READ)
 	if !file:
-		print_debug(_error + _titlePath)
+		print_debug(_error + _title_path)
 		return
 	while !file.eof_reached():
 		var csv := file.get_csv_line()
@@ -125,45 +125,45 @@ func _init() -> void:
 func _random(array: Array) -> String:
 	var size := array.size()
 	return "" if size == 0 else array[_rng.randi() % size]
-func _malePre() -> String: return _random(_data.male.pre)
-func _femalePre() -> String: return _random(_data.female.pre)
-func _neutralPre() -> String: return _random(_data.neutral.pre)
-func _maleStart() -> String: return _random(_data.male.start)
-func _femaleStart() -> String: return _random(_data.female.start)
-func _neutralStart() -> String: return _random(_data.neutral.start)
-func _maleMiddle() -> String: return _random(_data.male.middle)
-func _femaleMiddle() -> String: return _random(_data.female.middle)
-func _neutralMiddle() -> String: return _random(_data.neutral.middle)
-func _maleFinish() -> String: return _random(_data.male.finish)
-func _femaleFinish() -> String: return _random(_data.female.finish)
-func _neutralFinish() -> String: return _random(_data.neutral.finish)
-func _maleName() -> String: return _random(_data.male.name)
-func _femaleName() -> String: return _random(_data.female.name)
-func _neutralName() -> String: return _random(_data.neutral.name)
-func _malePost() -> String: return _random(_data.male.post)
-func _femalePost() -> String: return _random(_data.female.post)
-func _neutralPost() -> String: return _random(_data.neutral.post)
+func _male_pre() -> String: return _random(_data.male.pre)
+func _female_pre() -> String: return _random(_data.female.pre)
+func _neutral_pre() -> String: return _random(_data.neutral.pre)
+func _male_start() -> String: return _random(_data.male.start)
+func _female_start() -> String: return _random(_data.female.start)
+func _neutral_start() -> String: return _random(_data.neutral.start)
+func _male_middle() -> String: return _random(_data.male.middle)
+func _female_middle() -> String: return _random(_data.female.middle)
+func _neutral_middle() -> String: return _random(_data.neutral.middle)
+func _male_finish() -> String: return _random(_data.male.finish)
+func _female_finish() -> String: return _random(_data.female.finish)
+func _neutral_finish() -> String: return _random(_data.neutral.finish)
+func _male_name() -> String: return _random(_data.male.name)
+func _female_name() -> String: return _random(_data.female.name)
+func _neutral_name() -> String: return _random(_data.neutral.name)
+func _male_post() -> String: return _random(_data.male.post)
+func _female_post() -> String: return _random(_data.female.post)
+func _neutral_post() -> String: return _random(_data.neutral.post)
 
 func next() -> String:
 	var name := ""
-	var male := maleChance > _rng.randf()
-	var neutral := neutralEnable and neutralChance > _rng.randf()
-	if oneWord == false and preChance > _rng.randf():
-		name += _neutralPre() if neutral else _malePre() if male else _femalePre()
+	var male := male_chance > _rng.randf()
+	var neutral := neutral_enable and neutral_chance > _rng.randf()
+	if one_word == false and pre_chance > _rng.randf():
+		name += _neutral_pre() if neutral else _male_pre() if male else _female_pre()
 		name += " "
-		postChance += 0.5
-	if generatedEnable and generatedChance > _rng.randf():
-		name += _neutralStart() if neutral else _maleStart() if male else _femaleStart()
-		if vowelChance > _rng.randf():
+		post_chance += 0.5
+	if generated_enable and generated_chance > _rng.randf():
+		name += _neutral_start() if neutral else _male_start() if male else _female_start()
+		if vowel_chance > _rng.randf():
 			name += _random(_vowel)
-		if middle0Chance > _rng.randf():
-			name += _neutralMiddle() if neutral else _maleMiddle() if male else _femaleMiddle()
-		if middle1Chance > _rng.randf():
-			name += _neutralMiddle() if neutral else _maleMiddle() if male else _femaleMiddle()
-		name += _neutralFinish() if neutral else _maleFinish() if male else _femaleFinish()
+		if middle0_chance > _rng.randf():
+			name += _neutral_middle() if neutral else _male_middle() if male else _female_middle()
+		if middle1_chance > _rng.randf():
+			name += _neutral_middle() if neutral else _male_middle() if male else _female_middle()
+		name += _neutral_finish() if neutral else _male_finish() if male else _female_finish()
 	else:
-		name += _neutralName() if neutral else _maleName() if male else _femaleName()
-	if oneWord == false and postChance > _rng.randf():
+		name += _neutral_name() if neutral else _male_name() if male else _female_name()
+	if one_word == false and post_chance > _rng.randf():
 		name += " "
-		name += _neutralPost() if neutral else _malePost() if male else _femalePost()
+		name += _neutral_post() if neutral else _male_post() if male else _female_post()
 	return name
